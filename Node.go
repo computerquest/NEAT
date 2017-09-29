b6304d391b2719b329151c3903acafb1c2d2a107
Jared Stigter <jaredstigter@gmail.com>
2017-09-29 15:05:38 -0400

    worked on testing

--- Node.go
+++ Node.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"math"
-	"database/sql/driver"
 )
 
 type Node struct {
--- Node_Test.go
+++ Node_Test.go
@@ -4,25 +4,20 @@ import (
 	"testing"
 )
 
-func TestAddSendCon(t *testing.T) {
+func TestSendCon(t *testing.T) {
 	n := Node{send:make([]Connection, 3), receive: make([]*Connection, 3), numConIn: 0, numConOut: 0}
 
 	n.addSendCon(Connection{nodeFrom:&n})
-}
-func TestAddRecCon(t *testing.T) {
-	n := Node{send:make([]Connection, 3), receive: make([]*Connection, 3), numConIn: 0, numConOut: 0}
 
-	n.addRecCon(&Connection{})
-}
-
-func TestGetSendCon(t *testing.T) {
-	n := Node{send:make([]Connection, 3), receive: make([]*Connection, 3), numConIn: 0, numConOut: 0}
-
-	for i := 0; i < len(n.send); i++ {
-
+	if n.getSendCon(0).nodeFrom != &n || n.getSendCon(1).nodeFrom == &n {
+		t.Errorf("Node the correct node")
 	}
 }
+func TestRecCon(t *testing.T) {
+	n := Node{send:make([]Connection, 3), receive: make([]*Connection, 3), numConIn: 0, numConOut: 0}
 
-func TestGetRecCon(t *testing.T) {
-
+	n.addRecCon(&Connection{nodeTo: &n})
+	if n.getRecCon(0).nodeTo != &n {
+		t.Errorf("Node the correct node")
+	}
 }
